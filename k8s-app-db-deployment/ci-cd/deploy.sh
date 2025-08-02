0c5115ec65f3717ce6851b5bf46237fabf6c7524
Rchandu3447 <rajachandu.dev@gmail.com>
2025-08-02 08:16:17 +0530

    Update deploy.sh

--- k8s-app-db-deployment/ci-cd/deploy.sh
+++ k8s-app-db-deployment/ci-cd/deploy.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+set -e
 
-# Sample deploy script
-echo 'Running Terraform and Helm...'
+ENV=$1  # dev, staging, prod
+
+cd terraform
+terraform init
+terraform apply -var="env=$ENV" -var="region=us-east-1" \
+  -var="db_subnet_group=existing-subnet-group" \
+  -var="db_security_group_id=sg-xxxxxxx" -auto-approve
+
+cd ../helm
+
+# Deploy DB chart
+helm upgrade --install db-$ENV ./db-chart -f ../env/$ENV/values-db.yaml -n $ENV --create-namespace
+
+# Deploy App chart
+helm upgrade --install app-$ENV ./app-chart -f ../env/$ENV/values-app.yaml -n $ENV
